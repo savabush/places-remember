212372420417f51a55605727d7833e5ccb9849f8
savabush <vatka1337@gmail.com>
2022-04-29 13:16:22 +0700

    Changed to CBV view for adding delete func and add func of adding memories into ORM

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', include('social_django.urls', namespace='social')),
-    path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='index'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('addmemory/', views.AddMemory.as_view(), name='addmemory')
 ]
--- core/views.py
+++ core/views.py
@@ -1,27 +1,51 @@
-from django.shortcuts import render
+from core.handlers import vk_access_token
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.models import User
-from core.handlers import vk_access_token
+from .forms import MemoryForm
+from .models import Memory
+import re
 import secret
 
 
-def index(request):
-    if request.user.is_authenticated:
-        if not request.user.is_superuser:
-            vk_token = vk_access_token.get_access_token(request)[0]
-            owner_id = vk_access_token.get_access_token(request)[1]
-            memories_user = User.objects.get(id=request.user.id).memories.all()
-            return render(request, 'core/index.html', {'vk_access_token': vk_token,
-                                                       'owner_id_vk': owner_id,
-                                                       'memories': memories_user})
-    return render(request, 'core/index.html')
+class Index(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            if not request.user.is_superuser:
+                vk_token = vk_access_token.get_access_token(request)[0]
+                owner_id = vk_access_token.get_access_token(request)[1]
+                memories_user = User.objects.get(id=request.user.id).memories.all()
+                return render(request, 'core/index.html', {'vk_access_token': vk_token,
+                                                           'owner_id_vk': owner_id,
+                                                           'memories': memories_user})
+        return render(request, 'core/index.html')
+
+    def post(self, request):
+        id_memory = list(request.POST)[1]
+        Memory.objects.get(id=id_memory).delete()
+        vk_token = vk_access_token.get_access_token(request)[0]
+        owner_id = vk_access_token.get_access_token(request)[1]
+        memories_user = User.objects.get(id=request.user.id).memories.all()
+        return render(request, 'core/index.html', {'vk_access_token': vk_token,
+                                                   'owner_id_vk': owner_id,
+                                                   'memories': memories_user})
 
 
 class AddMemory(View):
+    form_class = MemoryForm
+    mapbox_key = secret.mapbox_key
+
     def get(self, request):
-        mapbox_key = secret.mapbox_key
-        return render(request, 'core/addmemory.html', {'mapbox_key': mapbox_key})
+        form = self.form_class()
+        return render(request, 'core/addmemory.html', {'mapbox_key': self.mapbox_key, 'form': form})
 
     def post(self, request):
-        print(request)
-        return index(request)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            points = re.findall(r'[0-9-\.0-9]+', request.GET.get('points'))
+            point_lng = points[0]
+            point_lat = points[1]
+            author = User.objects.get(id=request.user.id)
+            Memory.objects.create(**form.cleaned_data, author=author, point_lng=point_lng, point_lat=point_lat)
+            return redirect('/')
+        return render(request, 'core/addmemory.html', {'mapbox_key': self.mapbox_key, 'form': form})
