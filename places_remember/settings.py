76ad189ae16fab8440c941614129a76284c2f9a5
savabush <vatka1337@gmail.com>
2022-05-05 13:33:16 +0700

    DEBUG FALSE, corrected divs in index html

--- places_remember/settings.py
+++ places_remember/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secret.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'placesremember.ru']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
